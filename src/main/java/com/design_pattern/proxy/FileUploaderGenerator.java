51d3bf510bc6154ea86004881f8355eaedefdc6b
yh_m2l p <pyh_less15more@163.com>
2024-10-26 02:41:03 +0800

    add: proxy模式

--- src/main/java/com/design_pattern/proxy/FileUploaderGenerator.java
+++ src/main/java/com/design_pattern/proxy/FileUploaderGenerator.java
@@ -2,6 +2,7 @@ package com.design_pattern.proxy;
 
 import java.lang.reflect.Method;
 
+import net.sf.cglib.proxy.Enhancer;
 import net.sf.cglib.proxy.MethodInterceptor;
 import net.sf.cglib.proxy.MethodProxy;
 
@@ -11,11 +12,26 @@ import net.sf.cglib.proxy.MethodProxy;
  */
 public class FileUploaderGenerator implements MethodInterceptor{
 
-    
+    private Object tarObject;
+    public FileUploaderGenerator(Object tarObject) {
+        this.tarObject = tarObject;
+    }
+
+    public Object getProxy() {
+        Enhancer enhancer = new Enhancer();
+        // 通过字节码创建子类来实现代理（代理设置拦截器，对于父类的调用使用子类的实现）
+        enhancer.setSuperclass(tarObject.getClass());
+        enhancer.setCallback(this);
+        // 返回子类实例
+        return enhancer.create();
+    }
 
     @Override
     public Object intercept(Object arg0, Method arg1, Object[] arg2, MethodProxy arg3) throws Throwable {
-        
+        System.out.println("begin::");
+        Object invokeSuper = arg3.invokeSuper(arg0, arg2);
+        System.out.println("::end");
+        return invokeSuper;
     }
     
 }
--- src/main/java/com/design_pattern/proxy/Main.java
+++ src/main/java/com/design_pattern/proxy/Main.java
@@ -4,12 +4,14 @@ import java.lang.reflect.Proxy;
 
 public class Main {
     public static void main(String[] args) {
+        // 静态代理
         RealFileUploader realFileUploader = new RealFileUploader();
 
         FileUploader fileUploaderProxy = new FileUploaderProxy(realFileUploader);
 
         fileUploaderProxy.upload();
 
+        // jdk 动态代理
         DynamicProxyHandler handler = new DynamicProxyHandler(realFileUploader);
 
         FileUploader proxyFileUploader = (FileUploader) Proxy.newProxyInstance(
@@ -18,5 +20,11 @@ public class Main {
                 handler);
         
         proxyFileUploader.upload();
+
+        // CGLib 动态代理
+        FileUploaderGenerator proxyGenerator = new FileUploaderGenerator(realFileUploader);
+        FileUploader proxy = (FileUploader) proxyGenerator.getProxy();
+        proxy.upload();
+
     }
 }
