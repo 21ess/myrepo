d3dbde9832088f6d32ee7af961c4287850b44028
yh_m2l p <pyh_less15more@163.com>
2024-10-27 14:17:28 +0800

    åadd: huawei1024q1~3

---
+++ src/main/java/com/example/huawei/huawei1024q2.java
@@ -0,0 +1,9 @@
+package com.example.huawei;/**
+* 
+* @author alien
+* @program myrepo
+* @description 
+* @date 2024/10/26$
+*/
+public class huawei1024q2 {
+}
---
+++ src/main/java/com/example/huawei/huawei1024q3.java
@@ -0,0 +1,9 @@
+package com.example.huawei;/**
+* 
+* @author alien
+* @program myrepo
+* @description 
+* @date 2024/10/27$
+*/
+public class huawei1024q3 {
+}
